dc03a5538fca6be762a6f978744807d1aaa1951f
Xmegan <89880823+Xmegan@users.noreply.github.com>
2022-06-02 18:16:25 -1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// Toggle Click - Button
+$(document).ready(function() {
+    console.log("hello - you're looking at the console :)");
+    $("button").html ("Click me to display the Date and Time....")
+    $(".btn1").html ("Click once to make it disappear!")
+    $(".btn2").html ("I don't know why you'd want it to appear again, but click me.")
+    $("button").on("click", function(event) {
+        $("#date").toggleClass('warning');
+        console.log(" the times clicked on any buttons");
+        
+    });
+});
+
+
+$(document).ready(function(){
+        $(".btn1").click(function(){
+          $(".hate").fadeOut(1000);
+        });
+        $(".btn2").click(function(){
+          $(".hate").fadeIn(1000);
+        });
+      });
