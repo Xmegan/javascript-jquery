6cd7c138286c421e08e117da1615ebe146202c2a
Xmegan <89880823+Xmegan@users.noreply.github.com>
2022-06-05 12:27:20 -1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Toggle Click - Button
+// Fade In Button
+// Fade Out Button
 $(document).ready(function() {
     console.log("hello - you're looking at the console :)");
     $("button").html ("Click me to display the Date and Time....")
@@ -6,17 +8,12 @@ $(document).ready(function() {
     $(".btn2").html ("I don't know why you'd want it to appear again, but click me.")
     $("button").on("click", function(event) {
         $("#date").toggleClass('warning');
-        console.log(" the times clicked on any buttons");
-        
+        console.log(" the times clicked on any buttons");    
     });
+      $(".btn1").click(function(){
+      $(".hate").fadeOut(1000);
+    });
+    $(".btn2").click(function(){
+        $(".hate").fadeIn(1000);
+  })
 });
-
-
-$(document).ready(function(){
-        $(".btn1").click(function(){
-          $(".hate").fadeOut(1000);
-        });
-        $(".btn2").click(function(){
-          $(".hate").fadeIn(1000);
-        });
-      });
